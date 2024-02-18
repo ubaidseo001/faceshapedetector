49f4b7092fbf9aaf0be0ae17869053fc521bc425
ubaidseo001 <111786851+ubaidseo001@users.noreply.github.com>
2024-02-18 22:00:40 +0500

    Update home_page_script.js

--- home_page_script.js
+++ home_page_script.js
@@ -10,7 +10,7 @@ function onOpenCvReady() {
 
     // Detect faces
     let faceCascade = new cv.CascadeClassifier();
-    faceCascade.load('haarcascade_frontalface_default.xml');
+    faceCascade.load('/faceshapedetector/haarcascade_frontalface_default.xml');
     let faces = new cv.RectVector();
     let msize = new cv.Size(0, 0);
     faceCascade.detectMultiScale(gray, faces, 1.1, 3, 0, msize, msize);
